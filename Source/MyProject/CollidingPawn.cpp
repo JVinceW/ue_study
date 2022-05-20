171f82352b3fd2eff64e9969dd080ff65d85d6c5
JVince <Vincewang10a7@gmail.com>
2022-05-20 13:19:39 +0700

    Collision class and input setting

---
+++ Source/MyProject/CollidingPawn.cpp
@@ -0,0 +1,83 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "CollidingPawn.h"
+#include "UObject/ConstructorHelpers.h"
+#include "Particles/ParticleSystemComponent.h"
+#include "Components/SphereComponent.h"
+#include "Camera/CameraComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+
+
+// Sets default values
+ACollidingPawn::ACollidingPawn()
+{
+ 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	USphereComponent* SphereComponent = CreateDefaultSubobject<USphereComponent>(TEXT("RootComponent"));
+	RootComponent = SphereComponent;
+	SphereComponent-> InitSphereRadius(40.0f);
+	SphereComponent->SetCollisionProfileName(TEXT("Pawn"));
+
+	// Create Sphere Mesh to match the sphere component
+	UStaticMeshComponent* SphereVisualMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("VisualRepresentation"));
+	SphereVisualMesh->SetupAttachment(SphereComponent);
+	static  ConstructorHelpers::FObjectFinder<UStaticMesh> SphereVisualAsset(TEXT("/Game/StarterContent/Shapes/Shape_Sphere.Shape_Sphere"));
+	// According to the tutorial, the size of the sphere have to be set like this to fix the sphere component
+	if (SphereVisualAsset.Succeeded())
+	{
+		SphereVisualMesh->SetStaticMesh(SphereVisualAsset.Object);
+		SphereVisualMesh->SetRelativeLocation(FVector(0.0f,0.0f,-40.0f));
+		SphereVisualMesh->SetWorldScale3D(FVector(0.8f));
+	}
+
+	// Create Partical System
+	FirePartical = CreateDefaultSubobject<UParticleSystemComponent>(TEXT("MovePartical"));
+	FirePartical->SetupAttachment(SphereVisualMesh);
+	static ConstructorHelpers::FObjectFinder<UParticleSystem> FireParticleVisualAsset(TEXT("/Game/StarterContent/Particles/P_Fire.P_Fire"));
+	if (FireParticleVisualAsset.Succeeded())
+	{
+		FirePartical->SetTemplate(FireParticleVisualAsset.Object);
+	}
+
+	InitSpringArm();
+
+	AutoPossessPlayer = EAutoReceiveInput::Player0;
+}
+
+void ACollidingPawn::InitSpringArm()
+{
+	USpringArmComponent* SpringArmComponent = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraSpringArm"));
+	SpringArmComponent->SetupAttachment(RootComponent);
+	SpringArmComponent->SetRelativeRotation(FRotator(-45.0f, 0.0f, 0.0f));
+	SpringArmComponent->TargetArmLength = 400.0f;
+	SpringArmComponent->bEnableCameraLag = true;
+	SpringArmComponent->CameraLagSpeed = 3.0f;
+
+	// Create Camera into spring arm
+	UCameraComponent* CameraComponent = CreateDefaultSubobject<UCameraComponent>(TEXT("SpringArmCamera"));
+	CameraComponent->SetupAttachment(SpringArmComponent, USpringArmComponent::SocketName);
+}
+
+// Called when the game starts or when spawned
+void ACollidingPawn::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ACollidingPawn::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void ACollidingPawn::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
